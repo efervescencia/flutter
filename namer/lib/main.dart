251c5e0c8221df6c7bd07e4fc7d40893654dccf3
Julio Montero <juliointelek@hotmail.com>
2024-04-11 15:03:07 +0100

    added like button

--- namer/lib/main.dart
+++ namer/lib/main.dart
@@ -33,7 +33,19 @@ class MyAppState extends ChangeNotifier {
     current = WordPair.random();
     notifyListeners();
   }
+
+  var favorites = <WordPair>[];
+
+  void toggleFavorite() {
+    if (favorites.contains(current)) {
+      favorites.remove(current);
+    } else {
+      favorites.add(current);
+    }
+    notifyListeners();
+  }
 }
+
 
 
 class MyHomePage extends StatelessWidget {
@@ -41,6 +53,14 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var pair = appState.current;
+
+    IconData icon;
+    if (appState.favorites.contains(pair)) {
+      icon = Icons.favorite;
+    } else {
+      icon = Icons.favorite_border;
+    }
+
 
     return Scaffold(
       body: Center(
@@ -51,11 +71,25 @@ class MyHomePage extends StatelessWidget {
         
             SizedBox(height: 10),
             
-            ElevatedButton(
-              onPressed: () {
-                appState.getNext();  // ← This instead of print().
-              },
-              child: Text('Next'),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                ElevatedButton.icon(
+                  onPressed: () {
+                    appState.toggleFavorite();
+                  },
+                  icon: Icon(icon),
+                  label: Text('Like'),
+                ),
+                SizedBox(width: 10),
+
+                ElevatedButton(
+                  onPressed: () {
+                    appState.getNext();  // ← This instead of print().
+                  },
+                  child: Text('Next'),
+                ),
+              ],
             ),
         
           ],
